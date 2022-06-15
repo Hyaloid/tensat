50325eae5ee6b9e00eda9aa84dbd9db5f0099151
SeaMount <33570593+Hyaloid@users.noreply.github.com>
2022-06-15 14:37:11 +0800

    Update run_docker.sh

--- docker/run_docker.sh
+++ docker/run_docker.sh
@@ -1,7 +1,7 @@
 docker run --gpus all --pid=host --net=host \
 --name try \
 -it \
---mount type=bind,source="/home/yichenyang/tensat",target=/usr/tensat \
---mount type=bind,source="/home/yichenyang/egg",target=/usr/egg \
---mount type=bind,source="/home/yichenyang/taso",target=/usr/TASO \
-tensat:1.0 bash
+--mount type=bind,source="/home/zhaohs/tensat_test",target=/usr/tensat \
+--mount type=bind,source="/home/zhaohs/egg",target=/usr/egg \
+--mount type=bind,source="/home/zhaohs/TASO",target=/usr/TASO \
+tensat_test bash
