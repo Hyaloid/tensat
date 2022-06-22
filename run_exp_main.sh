2bf83807b1dff7489a45ee2ed1395209f6f7edf7
hs <haisha.zhao@gmail.com>
2022-06-22 23:05:04 +0800

    get optimized model

--- run_exp_main.sh
+++ run_exp_main.sh
@@ -18,7 +18,7 @@ models=(
 for pass in $(seq 0 $(expr $num_passes - 1)); do
     for iter_multi in 0 1 2; do
         for model in "${models[@]}"; do
-            cargo run --release -- -r converted.txt -t converted_multi.txt -u -s none --n_iter $iter_limit --no_order --ilp_time_sec 0 --no_cycle --iter_multi $iter_multi --n_sec $time_limit --n_nodes $node_limit --all_weight_only -e ilp -d $model -o tmp/"$model"_"$iter_multi"_stats.txt # -x tmp/"$model"_optimized.model
+            cargo run --release -- -r converted.txt -t converted_multi.txt -u -s none --n_iter $iter_limit --no_order --ilp_time_sec 0 --no_cycle --iter_multi $iter_multi --n_sec $time_limit --n_nodes $node_limit --all_weight_only -e ilp -d $model -o tmp/"$model"_"$iter_multi"_stats.txt -x tmp/"$model"_optimized.model
         done
     done
 done
